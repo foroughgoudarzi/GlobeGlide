329f81bbfffa2f7db1a6e06fb8731dc29ef91652
Gifty <giftyaningg@gmail.com>
2024-02-07 20:30:39 +0000

    Update photo.js

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -1,5 +1,9 @@
 // This script gets coordinates of a city and place and return a url to a photo of the place
-    
+ 
+     // fetchPlaces("glasgow", "monuments", "rating")
+     // sort: popular or newest  
+    // Gets city name and fetches coordinates of the city
+
 async function findPhoto(fsq_id){
 const options = {method: 'GET', headers: {accept: 'application/json', Authorization: 'fsq3uGJyqb8GYslDA9kCvLI2OQWCPzUPl2HYay7NK0Lzjpw=',}};
 
